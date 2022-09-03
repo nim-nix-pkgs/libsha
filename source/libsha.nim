c7dcd81f232fb70f9dc941731e122512bf131735
hugosenari <hugosenari@gmail.com>
2022-09-03 19:48:59 -0300

    feat(nimpkgs): update lock

--- source/libsha.nim
+++
@@ -1,2 +0,0 @@
-import libsha / [sha1, sha256, sha512, sha224, sha384, sha512m224, sha512m256]
-export sha1, sha256, sha512, sha224, sha384, sha512m224, sha512m256
--- source/libsha.nimble
+++
@@ -1,6 +0,0 @@
-version = "1.0"
-author = "Ievgen Svietikov"
-description = "Sha1 and Sha2 implementations"
-license = "MIT"
-
-skipDirs = @["tests"]
